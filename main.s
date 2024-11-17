22e9a1232f716f7ff678e54ca6a138d1b6f09d1e
Christinayj1102 <129067383+Christinayj1102@users.noreply.github.com>
2024-11-17 19:21:01 +0900

    Create main.s

---
+++ main.s
@@ -0,0 +1,165 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter the size of array: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Enter the elements: "
+.LC3:
+	.string	"Sorted array"
+.LC4:
+	.string	"%d "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$56, %rsp
+	.cfi_offset 3, -24
+	movq	%fs:40, %rax
+	movq	%rax, -24(%rbp)
+	xorl	%eax, %eax
+	movq	%rsp, %rax
+	movq	%rax, %rbx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-52(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-52(%rbp), %eax
+	movslq	%eax, %rdx
+	subq	$1, %rdx
+	movq	%rdx, -40(%rbp)
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movl	$16, %eax
+	subq	$1, %rax
+	addq	%rdx, %rax
+	movl	$16, %esi
+	movl	$0, %edx
+	divq	%rsi
+	imulq	$16, %rax, %rax
+	movq	%rax, %rcx
+	andq	$-4096, %rcx
+	movq	%rsp, %rdx
+	subq	%rcx, %rdx
+.L2:
+	cmpq	%rdx, %rsp
+	je	.L3
+	subq	$4096, %rsp
+	orq	$0, 4088(%rsp)
+	jmp	.L2
+.L3:
+	movq	%rax, %rdx
+	andl	$4095, %edx
+	subq	%rdx, %rsp
+	movq	%rax, %rdx
+	andl	$4095, %edx
+	testq	%rdx, %rdx
+	je	.L4
+	andl	$4095, %eax
+	subq	$8, %rax
+	addq	%rsp, %rax
+	orq	$0, (%rax)
+.L4:
+	movq	%rsp, %rax
+	addq	$3, %rax
+	shrq	$2, %rax
+	salq	$2, %rax
+	movq	%rax, -32(%rbp)
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, -48(%rbp)
+	jmp	.L5
+.L6:
+	movl	-48(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-32(%rbp), %rax
+	addq	%rdx, %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	addl	$1, -48(%rbp)
+.L5:
+	movl	-52(%rbp), %eax
+	cmpl	%eax, -48(%rbp)
+	jl	.L6
+	movl	-52(%rbp), %edx
+	movq	-32(%rbp), %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	bubbleSort@PLT
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, -44(%rbp)
+	jmp	.L7
+.L8:
+	movq	-32(%rbp), %rax
+	movl	-44(%rbp), %edx
+	movslq	%edx, %rdx
+	movl	(%rax,%rdx,4), %eax
+	movl	%eax, %esi
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	addl	$1, -44(%rbp)
+.L7:
+	movl	-52(%rbp), %eax
+	cmpl	%eax, -44(%rbp)
+	jl	.L8
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	movq	%rbx, %rsp
+	movq	-24(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L10
+	call	__stack_chk_fail@PLT
+.L10:
+	movq	-8(%rbp), %rbx
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.2.0-23ubuntu4) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
